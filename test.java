bf60d32f04816753a036c28c3c4b7f2327ffd0f8
TillJo <johanndeiter.till@gmail.com>
2017-09-18 21:18:05 +0200

    Codekk

---
+++ test.java
@@ -0,0 +1,42 @@
+public class raetsel {
+
+
+    public static int a;
+
+    public static void main(String[] args)
+
+    {
+
+        all(100000);
+    }
+
+    static void all (int y)
+    {
+        for (int j=1; j<=y; j++)
+        {
+            cip(j,0);
+        }
+    }
+
+    static void cip (int x,int z)
+    {
+
+        for(int i=1; i<=x&&z<3; i++)
+        {
+            a = x%i;
+            if (a==0)
+            {
+                ++z;
+
+            }
+
+        }
+            if (z==2)
+            {
+                System.out.println(x);
+            }
+
+
+    }
+
+}
